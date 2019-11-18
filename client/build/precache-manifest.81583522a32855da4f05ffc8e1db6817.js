c05fa77a69dc2934af6132a31043da6931bd7883
Preston Taylor <prestaylor51@gmail.com>
2019-11-17 17:30:34 -0700

    putting build in heroku

---
+++ client/build/precache-manifest.81583522a32855da4f05ffc8e1db6817.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc991a1474a31754e1b9ae766e71270d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "957b4b65759278016787",
+    "url": "/static/css/main.491bee12.chunk.css"
+  },
+  {
+    "revision": "a9c05c1c7af2b3c88d74",
+    "url": "/static/js/2.c58ba8dc.chunk.js"
+  },
+  {
+    "revision": "957b4b65759278016787",
+    "url": "/static/js/main.6ab1445c.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
