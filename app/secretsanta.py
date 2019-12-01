245ab9f7df1b94c7fd33ef0fa42566d20c62ca74
prestaylor51 <prestaylor51@gmail.com>
2019-12-01 14:14:40 -0700

    can't choose youself

--- app/secretsanta.py
+++ app/secretsanta.py
@@ -12,9 +12,6 @@ class SecretSantaService:
         # load givers
         self.givers = self.load_givers()
 
-        print('members from db: ')
-        print(self.givers)
-
         # Seed random generator
         self.random_gen = random
         self.random_gen.seed()
@@ -35,8 +32,7 @@ class SecretSantaService:
     def get_receiver_from_code(self, code):
         # get santa from code
         giver = self.get_giver_from_code(code)
-        print(giver)
-        print(f'santa: {giver[2]}')
+
         # from santa get match (from txt or make match)
         return self.determine_match(giver)
 
@@ -50,14 +46,12 @@ class SecretSantaService:
         cur.execute("select * from givers where code = %s", (upper_code,))
         giver = cur.fetchone()
         cur.close()
-        print(f'giver: {giver[2]}')
         return giver
 
     '''
     DERTERMINE MATCH
     '''
     def determine_match(self, giver):
-        print('called determine_match()')
         cur = self.conn.cursor()
 
         # get the match if exists
@@ -68,33 +62,28 @@ class SecretSantaService:
         cur.close()
 
         if row != None:
-            print('giver has receiver already')
             receiver = self.get_giver_by_id(row[1])
-            print(receiver)
             return receiver[2]
         else: 
             # if no match for giver then create one
-            print('giver has no receiver')
             return self.select_match_for_giver(giver)
 
     # returns name of selected receiver
     # use rowcount
     def select_match_for_giver(self, giver):
         cur = self.conn.cursor()
+
+        # get ids or potential receivers (can't get self)
         cur.execute('select g.id from givers g \
-                    where g.id not in (select receiver_id from giver_receiver);')
+                    where g.id not in (select receiver_id from giver_receiver) \
+                    and g.id <> %s;',(giver[0],))
         size = cur.rowcount
-        print(f'size: {size}')
-        rows = cur.fetchall()
+        potential_ids = cur.fetchall()
         cur.close()
-        print(f'rows: {rows}')
         ran_index = self.random_gen.randint(0,size - 1)
-        receiver_id = rows[ran_index][0]
-        print(f'index: {ran_index}')
-        print(f'row: {rows[ran_index]}')
+        receiver_id = potential_ids[ran_index][0]
 
         # write giver_receiver row for giver and id
-        print(f'new receiver id: {receiver_id}')
         self.save_giver_receiver(giver[0], receiver_id)
         return self.get_giver_by_id(receiver_id)[2]
 
