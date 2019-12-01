0e2165522d824d44065169301a75817ce4e9b35e
prestaylor51 <prestaylor51@gmail.com>
2019-12-01 13:44:36 -0700

    minor sql change

--- app/sql/schema.sql
+++ app/sql/schema.sql
@@ -10,7 +10,7 @@ create table giver_receiver
 (
     giver_id int references givers(id),
     receiver_id int references givers(id),
-    constraint unique_giver_receiver unique(giver, receiver)
+    constraint unique_giver_receiver unique(giver_id, receiver_id)
 );
 
 insert into givers
